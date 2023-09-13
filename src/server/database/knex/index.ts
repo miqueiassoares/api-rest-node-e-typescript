e2d6cca481c9b884753c4b22194ed1c261e2fd25
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-09-13 20:19:02 -0300

    fix: type number id

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,11 @@
 import knex from 'knex';
+import pg from 'pg';
 import 'dotenv/config';
 import { development, production, test } from './Enviroment';
+
+if (process.env.NODE_ENV === 'production') {
+  pg.types.setTypeParser(20, 'text', parseInt);
+}
 
 const getEnviroment = () => {
   switch (process.env.NODE_ENV) {
